9e7b2d2444b820587dd249e108f42d5bff509617
Pedro Pessoa <pessoa.pedro91@gmail.com>
2017-11-12 22:52:39 +0200

    Create Input component

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,18 +1,29 @@
 import React from 'react';
-import { View } from 'react-native';
-import { Card, CardItem, Button } from './common';
+import { } from 'react-native';
+import { Card, CardItem, Button, Input } from './common';
 
 class LoginForm extends React.Component {
+
+  state = {
+    emailText: '',
+    passwordText: '',
+  };
+
   render() {
     return (
       <Card>
-        <CardItem />
-        <CardItem />
-
         <CardItem>
-          <Button>
-            Log in
-            </Button>
+          <Input
+            autoCorrect={false}
+            placeholder="user@email.com"
+            label="Email"
+            value={this.state.emailText}
+            onChangeText={emailText => this.setState({ emailText })}
+          />
+        </CardItem>
+        <CardItem />
+        <CardItem>
+          <Button> Log in </Button>
         </CardItem>
       </Card>
     );
--- src/components/common/index.js
+++ src/components/common/index.js
@@ -2,3 +2,4 @@ export * from './Header';
 export * from './Card';
 export * from './CardItem';
 export * from './Button';
+export * from './Input';
