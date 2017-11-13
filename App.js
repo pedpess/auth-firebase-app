be33cb0d0ef8cb9689cc6ff5302e97d0ba0c3c19
Pedro Pessoa <pessoa.pedro91@gmail.com>
2017-11-13 12:04:26 +0200

    Add manipulation of state of the user for rendering

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './src/components/common';
+import { Header, Button, Spinner, CardItem } from './src/components/common';
 import LoginForm from './src/components/LoginForm';
 
 export default class App extends React.Component {
+
+  state = {
+    loggedIn: null,
+  }
 
   componentWillMount() {
     firebase.initializeApp({
@@ -15,13 +19,36 @@ export default class App extends React.Component {
       storageBucket: 'auth-firebase-app-8b7c5.appspot.com',
       messagingSenderId: '888433026715'
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <CardItem>
+            <Button onPress={() => firebase.auth().signOut()}>Log out</Button>
+          </CardItem>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size={'large'} />;
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText='Authentication' />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
